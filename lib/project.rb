df1249667b232820e798f9242e1c84d4d3ea3eb1
imperative-genre-4923 <dreama.lin@yale.edu>
2020-06-14 22:08:35 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,8 +1,12 @@
 class Project
 
-  attr_reader :backer
+  attr_reader :backer, :title
 
   @backers = []
+
+  def initialized(title)
+    @title=title
+  end
 
   def add_backer(backer)
     @backers << backer
